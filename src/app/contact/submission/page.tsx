360b5346e3dba06ee2de695b009688583b448a85
Redreev8 <redreevs@mail.ru>
2025-03-29 02:20:13 +0500

    fix remove console.log check riderec submit.tsx

--- src/app/contact/submission/page.tsx
+++ src/app/contact/submission/page.tsx
@@ -7,8 +7,6 @@ import { redirect } from 'next/navigation'
 export default async function Submission() {
     const headersList = await headers()
     const referer = headersList.get('referer')
-    console.log(referer)
-    console.log(referer.match(/\/contact(\/?)$/g))
     if (!referer || !referer.match(/\/contact(\/?)$/g)) {
         redirect('/contact')
     }
