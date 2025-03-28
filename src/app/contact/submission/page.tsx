1373520bdf86b19e4fea1f38837204841ca9a1c5
Redreev8 <redreevs@mail.ru>
2025-03-28 21:33:16 +0500

    add page submission

---
+++ src/app/contact/submission/page.tsx
@@ -0,0 +1,13 @@
+import Container from '@/ui/container'
+import Section from '@/ui/section'
+import Title from '@/ui/title'
+
+export default function Submission() {
+    return (
+        <Section>
+            <Container>
+                <Title as="h1">Message generated on the server</Title>
+            </Container>
+        </Section>
+    )
+}
