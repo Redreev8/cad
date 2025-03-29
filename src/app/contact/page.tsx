093f71295d526e828e7b138a8da9035b41ebb3bb
Redreev8 <redreevs@mail.ru>
2025-03-29 13:16:10 +0500

    add metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,22 @@
+import { Metadata } from 'next'
 import ContactForm from './components/contact-form'
+
+const title = 'contacts page tacks'
+const description = 'contacts page written in next js, scss, radix form'
+
+export const metadata: Metadata = {
+    title: 'Contacts Some Company',
+    description:
+        'Contacts Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mattis, leo et condimentum',
+    openGraph: {
+        title,
+        description,
+    },
+    twitter: {
+        title,
+        description,
+    },
+}
 
 export default function Contact() {
     return <ContactForm />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+import { Metadata } from 'next'
 import HomeAlso from './components/home-also'
 import HomeIntro from './components/home-intro'
 import HomeLess from './components/home-less'
+
+const title = 'home page tacks'
+const description = 'home page written in next js, scss, radix form'
+
+export const metadata: Metadata = {
+    title: 'Some Company',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mattis, leo et condimentum',
+    openGraph: {
+        title,
+        description,
+    },
+    twitter: {
+        title,
+        description,
+    },
+}
 
 export default function Home() {
     return (
